fd28113c03269ef5bccc96b829844e30e215c77f
HeadlessHamster <joost_rietdijk@live.nl>
2024-01-06 12:45:47 +0100

    Added player class

---
+++ lib/classes/player.dart
@@ -0,0 +1,20 @@
+class Player {
+  final String playerID;
+  String name = "";
+  int legsWon = 0;
+  int legsLost = 0;
+  int pointsDifference = 0;
+  int gamesPlayed = 0;
+  double legAverage = 0;
+  double tournamentAverage = 0;
+
+  Player({required this.playerID, required this.name});
+
+  List<dynamic> convertToArray() {
+    return [name, legsWon, legsLost, gamesPlayed];
+  }
+
+  void calculatePointsDifference() {
+    pointsDifference = legsWon - legsLost;
+  }
+}
