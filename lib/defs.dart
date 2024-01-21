5d796b8a36f8140cad68f98fd8a29a7421a3d424
HeadlessHamster <joost_rietdijk@live.nl>
2024-01-21 16:08:40 +0100

    Returned to previous version

--- lib/defs.dart
+++ lib/defs.dart
@@ -4,14 +4,13 @@ const Color cardOutlineColor = Color.fromARGB(95, 10, 10, 10);
 const Color cardBackground = Color.fromRGBO(36, 36, 36, 1);
 const Color flushbarGreen = Color.fromRGBO(56, 142, 60, 1);
 const Color flushbarRed = Color.fromRGBO(211, 47, 47, 1);
-const List<int> appVersion = [4, 4, 0];
+const List<int> appVersion = [4, 3, 0];
 
 const List<String> supportedSaveGameVersions = [
   "4.0.0",
   "4.1.0",
   "4.2.0",
-  "4.3.0",
-  "4.4.0"
+  "4.3.0"
 ];
 const List<String> specialSounds = ["006", "020", "023", "042", "063", "064"];
 
